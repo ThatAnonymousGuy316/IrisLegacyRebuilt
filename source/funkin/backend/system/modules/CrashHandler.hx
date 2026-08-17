103472ba026b41c49d4b8c07b0f3fdb95f5c58b9
dumbassosolol <ccager7@gmail.com>
2026-08-17 12:45:25 -0400

    added a crash handler that you prob will NOT see 😊

--- source/funkin/backend/system/modules/CrashHandler.hx
+++ source/funkin/backend/system/modules/CrashHandler.hx
@@ -51,7 +51,7 @@ final class CrashHandler {
 		e.stopPropagation();
 		e.stopImmediatePropagation();
 
-		NativeAPI.showMessageBox("Codename Engine Crash Handler", 'Uncaught Error:$m\n\n$stackLabel', MSG_ERROR);
+		NativeAPI.showMessageBox("Diddy touches me at night pls help :cold_sweat:", 'Uh Oh Stinkyy:$m\n\n$stackLabel', MSG_ERROR);
 		#if sys
 		Sys.exit(1);
 		#end
